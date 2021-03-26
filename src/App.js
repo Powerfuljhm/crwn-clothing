2bcc0bde97c94981e6825be84a59047fc719ac1d
Justin Martinez <powerfuljhm@gmail.com>
2021-03-25 18:50:25 -0700

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './homepage.styles.scss';
+import './pages/homepage/homepage.styles.scss';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return <div>
